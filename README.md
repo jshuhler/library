e76b11a76c1e1ab1d5845c14b426a1fc8ae541f3
jshuhler <15068877+jshuhler@users.noreply.github.com>
2026-02-11 18:01:07 -0500

    created book constructor function

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 Working on the library project from [The Odin Project](https://www.theodinproject.com/lessons/node-path-javascript-library)'s Javascript course. 
 
 [Live preview](https://jshuhler.github.io/library/)
+
+## working notes
+### to do
+- [X] come up with a way to create a unique book title given an input name and a unique ID (`crypto.randomUUID()`)
+- [X] push book objects into the myLibrary array
+    - [this helped](https://stackoverflow.com/questions/42866809/push-to-array-from-constructor-in-plain-javascript)
+- [ ] split the above add to library array piece into a separate function so it can be called individually.
+
+### books
+- title
+- author
+- page count
+- read
+- id
---
+++ script.js
@@ -0,0 +1,32 @@
+// CREATING BASE ELEMENTS
+const myLibrary = [];
+
+// constructor for creating a book object
+function Book(title, author, page, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor.");
+    }
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.read = read;
+    this.id = crypto.randomUUID()
+    // this.addToLibrary = function () {
+    //     myLibrary.push(this);
+    // }
+    // this.addToLibrary();
+}
+
+// adding a book created above to the myLibrary array
+function addToLibrary () {
+    myLibrary.push(Book);
+}
+
+// create unique book title
+
+// TESTING
+console.log(myLibrary);
+const theHobbit = new Book("The Hobbit","JRR Tolkien","295","read");
+const annihilation = new Book("Annihilation","Jeff VanderMeer","208","unread");
+console.log(theHobbit);
+console.log(myLibrary);
