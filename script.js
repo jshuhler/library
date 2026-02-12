356f6befaa738ff28f11bbe4ca1c2f8fa67dbc11
jshuhler <15068877+jshuhler@users.noreply.github.com>
2026-02-12 15:46:07 -0500

    updateLibraryPage, favicon, add book button

--- script.js
+++ script.js
@@ -3,49 +3,55 @@ const myLibrary = [];
 
 // VARIABLES
 let book;
-const bookContainer = document.querySelectorAll(".book-container")
+const bookContainer = document.querySelector(".book-container");
+const newBookBtn = document.getElementById('add-book-btn');
 
-
-// constructor for creating a book object
-function Book(title, author, page, read) {
+// CREATE A BOOK CONSTRUCTOR
+function Book(title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor.");
     }
     this.title = title;
     this.author = author;
-    this.page = page;
+    this.pages = pages;
     this.read = read;
     this.id = crypto.randomUUID()
-    // this.addToLibrary = function () {
-    //     myLibrary.push(this);
-    // }
-    // this.addToLibrary();
 }
 
-// adding a book created above to the myLibrary array
-function addToLibrary(title, author, page, read) {
-    const newBook = new Book(title,author,page,read)
+
+// ADDING A BOOK TO LIBRARY ARRAY
+function addToLibrary(title, author, pages, read) {
+    const newBook = new Book(title,author,pages,read)
     myLibrary.push(newBook);
 }
 
-// adding books to DOM - looping through the myLibrary array and pulling each book out
-// I think this will eventually be on an event listener, so not adding [assed [arameters right now
+// UPDATE LIBRARY DISPLAY
 function updateLibraryPage(myLibrary) {
-    // for (let i = 0; i < myLibrary.length; i++) {
-    //     bookContainer.textContent = myLibrary[i];
-    // }
     for (const book of myLibrary) {
+        const bookCard = document.createElement("div");
+        bookCard.classList = "book-card";
+        bookCard.style.cssText = "padding-bottom: 20px";
+        bookContainer.appendChild(bookCard);
         for (const key in book) {
-            console.log(`${key}: ${book[key]}`);
+            const bookValue = document.createElement("div");
+            bookValue.classList = "book-value";
+            // bookTitle.style.cssText placeholder
+            bookCard.appendChild(bookValue);
+            bookValue.textContent = `${key}: ${book[key]}`;
         }
     }
 }
 
+// NEW BOOK BUTTON
+newBookBtn.addEventListener('click',() => {
+    dialog.showModal();
+});
+
 // TESTING
 // const theHobbit = new Book("The Hobbit","JRR Tolkien","295","read");
 // const annihilation = new Book("Annihilation","Jeff VanderMeer","208","unread");
-// console.log(theHobbit);
-addToLibrary("Annihilation","Jeff VanderMeer","208","unread");
+addToLibrary("House of Leaves","Mark Z. Danielewski","709","unread")
+addToLibrary("Annihilation","Jeff VanderMeer","208","read");
 addToLibrary("The Hobbit","JRR Tolkien","295","read");
 console.log(myLibrary);
 updateLibraryPage(myLibrary);
